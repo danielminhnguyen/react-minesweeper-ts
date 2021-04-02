12b09119c93d8adf710de68221864fd30b1f7d34
DANIEL NGUYEN <hokkinxx@DANIELs-MBP.lan>
2021-04-02 19:39:15 +1300

    finish generate cells logic

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,46 @@
 import React from "react";
+import { CellState, CellValue } from "../../types";
 
 import "./Button.scss";
 
-const Button: React.FC = () => {
-  return <div className="Button" />;
+interface ButtonProps {
+  row: number;
+  col: number;
+  state: CellState;
+  value: CellValue;
+}
+
+const Button: React.FC<ButtonProps> = ({ row, col, state, value }) => {
+  const renderContent = (): React.ReactNode => {
+    if (state === CellState.Visible) {
+      if (value === CellValue.Bomb) {
+        return (
+          <span role="image" aria-label="bomb">
+            💣
+          </span>
+        );
+      } else if (value === CellValue.None) {
+        return null;
+      }
+
+      return value;
+    } else if (state === CellState.Flagged) {
+      return (
+        <span role="image" aria-label="flagged">
+          ⛳️
+        </span>
+      );
+    }
+  };
+  return (
+    <div
+      className={`Button ${
+        state === CellState.Visible ? "visible" : ""
+      } value-${value}`}
+    >
+      {renderContent()}
+    </div>
+  );
 };
 
 export default Button;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,10 @@
-import { MAX_COLS, MAX_ROWS } from "../constants";
+import { MAX_COLS, MAX_ROWS, NO_OF_BOMBS } from "../constants";
 import { Cell, CellState, CellValue } from "../types";
 
 export const generateCells = (): Cell[][] => {
   const cells: Cell[][] = [];
 
+  // generate all cells
   for (let row = 0; row < MAX_ROWS; row++) {
     cells.push([]);
     for (let col = 0; col < MAX_COLS; col++) {
@@ -11,6 +12,94 @@ export const generateCells = (): Cell[][] => {
         value: CellValue.None,
         state: CellState.Open,
       });
+    }
+  }
+
+  // randomly put 10 bombs
+  let bombsPlaced = 0;
+  while (bombsPlaced < NO_OF_BOMBS) {
+    const randomRow = Math.floor(Math.random() * MAX_ROWS);
+    const randomCol = Math.floor(Math.random() * MAX_COLS);
+
+    const currentCell = cells[randomRow][randomCol];
+    if (currentCell.value !== CellValue.Bomb) {
+      cells[randomRow][randomCol] = {
+        ...cells[randomRow][randomCol],
+        value: CellValue.Bomb,
+      };
+      bombsPlaced++;
+    }
+  }
+
+  // calculate the number for each cells
+  for (let rowIndex = 0; rowIndex < MAX_ROWS; rowIndex++) {
+    for (let colIndex = 0; colIndex < MAX_COLS; colIndex++) {
+      const currentCell = cells[rowIndex][colIndex];
+      if (currentCell.value === CellValue.Bomb) {
+        continue;
+      }
+
+      let numberOfBombs = 0;
+      const topLeftBomb =
+        rowIndex > 0 && colIndex > 0 ? cells[rowIndex - 1][colIndex - 1] : null;
+      const topBomb = rowIndex > 0 ? cells[rowIndex - 1][colIndex] : null;
+      const topRightBomb =
+        rowIndex > 0 && colIndex < MAX_COLS - 1
+          ? cells[rowIndex - 1][colIndex + 1]
+          : null;
+      const leftBomb = colIndex > 0 ? cells[rowIndex][colIndex - 1] : null;
+      const rightBomb =
+        colIndex < MAX_COLS - 1 ? cells[rowIndex][colIndex + 1] : null;
+      const bottomLeftBomb =
+        rowIndex < MAX_ROWS - 1 && colIndex > 0
+          ? cells[rowIndex + 1][colIndex - 1]
+          : null;
+      const bottomBomb =
+        rowIndex < MAX_ROWS - 1 ? cells[rowIndex + 1][colIndex] : null;
+      const bottomRightBomb =
+        rowIndex < MAX_ROWS - 1 && colIndex < MAX_COLS - 1
+          ? cells[rowIndex + 1][colIndex + 1]
+          : null;
+
+      if (topLeftBomb?.value === CellValue.Bomb) {
+        numberOfBombs++;
+      }
+
+      if (topBomb?.value === CellValue.Bomb) {
+        numberOfBombs++;
+      }
+
+      if (topRightBomb?.value === CellValue.Bomb) {
+        numberOfBombs++;
+      }
+
+      if (leftBomb?.value === CellValue.Bomb) {
+        numberOfBombs++;
+      }
+
+      if (rightBomb?.value === CellValue.Bomb) {
+        numberOfBombs++;
+      }
+
+      if (bottomLeftBomb?.value === CellValue.Bomb) {
+        numberOfBombs++;
+      }
+
+      if (bottomBomb?.value === CellValue.Bomb) {
+        numberOfBombs++;
+      }
+
+      if (bottomRightBomb?.value === CellValue.Bomb) {
+        numberOfBombs++;
+      }
+
+      if (numberOfBombs > 0) {
+        cells[rowIndex][colIndex] = {
+          ...currentCell,
+          value: numberOfBombs,
+        };
+        console.log(cells[rowIndex][colIndex]);
+      }
     }
   }
 
